e2aebdfc3e2badd5ede4777061304db4a895849e
John Rykken and Kayla Ondracek <john@>
2018-03-13 13:54:36 -0700

    fix interface elements

--- src/main.js
+++ src/main.js
@@ -36,8 +36,24 @@ $(document).ready(function(){
     currentPlayer.bank = 0;
     $("#bankTotal" + player).text(currentPlayer.bank);
     $("#scoreTotal" + player).text(currentPlayer.total);
-    player = currentPlayer.changePlayer();
-    $("#playerTurn").html("<h3>Player " + player + " Turn</h3>")
+    if (currentPlayer.total >= 30)
+    {
+      $("#playerTurn").html("<h3>Player " + player + " WINS!!!!!</h3>")
+      player1.bank = 0;
+      player1.total = 0;
+      player2.bank = 0;
+      player2.total = 0;
+      $('#bankTotal1').text("YOU ARE AMAZING!!!");
+      $('#bankTotal2').text("YOU SUCK!!!");
+      $('#scoreTotal1').text(0);
+      $('#scoreTotal2').text(0); 
+    }
+    else
+    {
+      player = currentPlayer.changePlayer();
+      $("#playerTurn").html("<h3>Player " + player + " Turn</h3>")
+    }
+
   });
 
   //hook up hold functionality and adding to total
